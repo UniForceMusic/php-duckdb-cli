088720f53c62cf23b326ea94fc2521553c2c9501
UniForceMusic <51342446+UniForceMusic@users.noreply.github.com>
2026-02-03 00:11:15 +0100

    Improve results class

--- src/Integrations/Sentience/DuckDBResult.php
+++ src/Integrations/Sentience/DuckDBResult.php
@@ -2,30 +2,17 @@
 
 namespace UniForceMusic\PHPDuckDBCLI\Integrations\Sentience;
 
-use Sentience\Database\Results\ResultAbstract;
+use Sentience\Database\Results\Result;
 use UniForceMusic\PHPDuckDBCLI\Results\ResultInterface;
 
-class DuckDBResult extends ResultAbstract
+class DuckDBResult extends Result
 {
-    private array $rows = [];
+    protected array $columns;
+    protected array $rows;
 
     public function __construct(private ResultInterface $result)
     {
+        $this->columns = $result->getColumns();
         $this->rows = $result->getRows();
-    }
-
-    public function columns(): array
-    {
-        return $this->result->getColumns();
-    }
-
-    public function fetchAssoc(): ?array
-    {
-        return $this->rows[0] ?? null;
-    }
-
-    public function fetchAssocs(): array
-    {
-        return $this->rows;
     }
 }
